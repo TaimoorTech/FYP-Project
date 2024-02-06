909866be07ad3b6f7a9d3e0e9632e27998e3adac
TaimoorTech <smtaimoor13@gmail.com>
2024-02-06 23:33:37 +0500

    Login Hive Database Implemented

---
+++ lib/dataSources/localDataSource/loginHive/userDatabase.dart
@@ -0,0 +1,65 @@
+import 'package:fyp_project/dataSources/localDataSource/loginHive/userHiveBox.dart';
+import 'package:hive/hive.dart';
+import 'package:intl/intl.dart';
+
+import '../../../modelClasses/userModel.dart';
+
+part 'userDatabase.g.dart';
+
+@HiveType(typeId: 1)
+class UserDatabase{
+
+  UserDatabase({
+    required this.email,
+    required this.password,
+  });
+
+  @HiveField(0)
+  String email;
+
+  @HiveField(1)
+  String password;
+
+  static int addUser(User newUser){
+    Map<String, dynamic> values = newUser.toMap();
+    String _email = values['email'];
+    String _password = values['password'];
+    try{
+      userBox.put('$_email', UserDatabase(email: _email, password: _password));
+      return 1;
+    }
+    catch(e){
+      return 0;
+    }
+
+  }
+
+  static Future<List<User>> getUser() async {
+    // final Map<dynamic, dynamic> result = taskBox.g;
+    final result = userBox.toMap().map(
+          (k, e) => MapEntry(
+          k.toString(), UserDatabase.conversion(e)
+      ),
+    );
+    List<Map<String, dynamic>> userDetails = [];
+    result.values.forEach((v) => userDetails.add(v));
+    // taskBox.ge
+    return userDetails.map(User.fromMap).toList();
+  }
+
+  static Map<String, dynamic> conversion(UserDatabase db_val) {
+    String _email = db_val.email;
+    String _password = db_val.password;
+
+    Map<String, dynamic> map = {'email': _email, 'password': _password};
+
+    return map;
+  }
+
+  static Future<void> clearUser() async {
+    userBox.clear();
+  }
+
+}
+
+
