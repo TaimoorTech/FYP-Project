13564ac11ea0c250f0ff2be9964a5967113673e2
TaimoorTech <smtaimoor13@gmail.com>
2024-02-13 00:04:53 +0500

    "Changes in Duration of SnackBar"

--- lib/utils/util.dart
+++ lib/utils/util.dart
@@ -4,21 +4,21 @@ import 'package:flutter/material.dart';
 class Util {
   static void normalSnackBar(BuildContext contexts, String textMsg){
     SnackBar snackBar = SnackBar(content: Text(textMsg,
-        style: const TextStyle(color: Colors.white)), elevation: 5,
+        style: const TextStyle(color: Colors.white)), elevation: 5, duration: Duration(seconds: 1),
         backgroundColor: Colors.black);
     ScaffoldMessenger.of(contexts).showSnackBar(snackBar);
   }
 
   static void submittedSnackBar(BuildContext contexts, String textMsg){
     SnackBar snackBar = SnackBar(content: Text(textMsg,
-        style: const TextStyle(color: Colors.white)), elevation: 5,
+        style: const TextStyle(color: Colors.white)), elevation: 5, duration: Duration(seconds: 1),
         backgroundColor: Colors.green);
     ScaffoldMessenger.of(contexts).showSnackBar(snackBar);
   }
 
   static void errorSnackBar(BuildContext contexts, String errorMsg){
     SnackBar snackBar = SnackBar(content: Text(errorMsg,
-        style: const TextStyle(color: Colors.white)), elevation: 5,
+        style: const TextStyle(color: Colors.white)), elevation: 5, duration: Duration(seconds: 1),
         backgroundColor: Colors.red);
     ScaffoldMessenger.of(contexts).showSnackBar(snackBar);
   }
